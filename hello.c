46ce9c90f3ab9b95f497ebee01b6de60dcbfe0ed
Ch4s3d <ramozchris@gmail.com>
2022-10-07 15:44:29 -0500

    error: create error for hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-//Random Change
+// Random Change
 
 int main(void)
 {
+    // Random comment
     printf("Hola Mundo");
+    decirHola("Daniel");
+    printf("Adios mundo");
     return 0;
 }
